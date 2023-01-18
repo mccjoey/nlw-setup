9d443e89cbfb18bc437a0b9cf7b020df30cd9c3b
Mccjoey <crash.oliver@gmail.com>
2023-01-18 16:03:54 +0000

    Class 2 - Server / Web Updates

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,23 +1,12 @@
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
-import { PrismaClient } from '@prisma/client';
+import { appRoutes } from './routes';
 
 const app = Fastify();
-const prisma = new PrismaClient();
 
 app.register(cors);
 
-app.get('/hello', async () => {
-   const habits = await prisma.habit.findMany({
-    where: {
-        title: {
-            startsWith: "Beber"
-        }
-    }
-   })
-
-   return habits
-})
+app.register(appRoutes);
 
 app.listen({
     port: 3333
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,14 @@
 import "./styles/global.css";
-import { Habit } from "./components/Habit";
+import { Header } from "./components/Header";
+import { SummaryTable } from "./components/SummaryTable";
 
-function App() {
-  return <Habit completed={3} />;
+export function App() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+        <SummaryTable />
+      </div>
+    </div>
+  );
 }
-
-export default App;
