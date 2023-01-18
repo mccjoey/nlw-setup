28ad144e20840044e7588b900476e1c5e707d158
Mccjoey <crash.oliver@gmail.com>
2023-01-18 19:56:54 +0000

    Class 2 - Mobile Updates

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -7,6 +7,7 @@ import {
   Inter_800ExtraBold,
 } from '@expo-google-fonts/inter';
 import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/Home';
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -21,26 +22,13 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Irru</Text>
+    <>
+      <Home />
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090A',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#fff',
-    fontFamily: 'Inter_800ExtraBold',
-  },
-});
